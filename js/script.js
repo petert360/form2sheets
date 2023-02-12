b9dc9e90db1570828ddeb2fbfb6d6ab4dcda147d
petert360 <peter.t360@hotmail.com>
2023-02-12 22:42:10 +0100

    add: JS alert box

--- js/script.js
+++ js/script.js
@@ -5,12 +5,19 @@ const form = document.forms['submit-to-google-sheet'];
 form.addEventListener('submit', e => {
   e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message));
+    .then(response => {
+      console.log('Success!', response);
+      window.alert('Sikeres küldés!');
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      window.alert('Hibás küldés!');
+    });
 });
 
 const selectElement = document.querySelector('.inputStartElement');
 
+/*
 selectElement.addEventListener('change', event => {
   const result = document.querySelector('.inputEndElement');
   
@@ -18,3 +25,4 @@ selectElement.addEventListener('change', event => {
   var newDate = new Date(selectElement.value);
   result.value = selectElement.value;
 });
+*/
